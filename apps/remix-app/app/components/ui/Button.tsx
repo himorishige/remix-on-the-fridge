80eca278d1c7325cb8fed94fe3fc6fea25539f78
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-20 16:41:51 +0900

    fix: Button width

--- apps/remix-app/app/components/ui/Button.tsx
+++ apps/remix-app/app/components/ui/Button.tsx
@@ -1,12 +1,16 @@
 import type { ComponentProps } from 'react';
+import { classNames } from '~/utils';
 
-type Props = Omit<ComponentProps<'button'>, 'className'>;
+type Props = Omit<ComponentProps<'button'>, 'className'> & { full?: boolean };
 
 export const Button: React.FC<Props> = (props) => {
   return (
     <button
       {...props}
-      className="py-2 px-6 text-lg text-white bg-blue-500 hover:bg-blue-600 rounded border-0 focus:outline-none"
+      className={classNames(
+        'py-2 px-6 text-lg text-white bg-blue-500 hover:bg-blue-600 disabled:bg-slate-400 rounded border-0 focus:outline-none',
+        props.full ? 'w-full' : '',
+      )}
     >
       {props.children}
     </button>
