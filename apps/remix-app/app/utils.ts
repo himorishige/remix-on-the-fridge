b33319bcfbf4f5d39952114ff97764e8e7697a16
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-20 16:41:09 +0900

    add: utils

--- apps/remix-app/app/utils.ts
+++ apps/remix-app/app/utils.ts
@@ -1,6 +1,14 @@
-export const normalizeRoomName = (room: string) => {
-  return room
+export const normalizeBoardName = (board: string) => {
+  return board
     .replace(/[^a-zA-Z0-9_-]/g, '')
     .replace(/_/g, '-')
     .toLowerCase();
+};
+
+export const timestampToLocalString = (timestamp: number) => {
+  return new Date(timestamp).toLocaleString();
+};
+
+export const classNames = (...classes: unknown[]): string => {
+  return classes.filter(Boolean).join(' ');
 };
