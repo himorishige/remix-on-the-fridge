6ce5b37f539be079eb1369c260bb9d3fae178b61
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-20 16:42:59 +0900

    update: BoardCard component

--- apps/remix-app/app/components/elements/BoardCard.tsx
+++ apps/remix-app/app/components/elements/BoardCard.tsx
@@ -1,27 +1,47 @@
 import type { Message } from 'board-do';
+import { classNames, timestampToLocalString } from '~/utils';
 
 type Props = {
   message: Message;
+  isMe: boolean;
 };
 
-export const BoardCard: React.FC<Props> = ({ message }) => {
+export const BoardCard: React.FC<Props> = ({ message, isMe }) => {
   return (
     <div
-      className="p-4 w-full md:w-1/2 lg:w-1/3"
+      className="p-2 w-full md:w-1/2 lg:w-1/3"
       key={`${message.timestamp}${message.name}${message.message}`}
     >
-      <div className="p-6 rounded-lg border border-gray-200">
-        <div className="inline-flex justify-center items-center mb-4 w-12 h-12 text-indigo-500 bg-indigo-100 rounded-full">
-          <img
-            src={`https://avatars.dicebear.com/api/pixel-art/${message.name}.svg`}
-            alt={message.name}
-            className="overflow-hidden w-12 h-12 rounded-full"
-          />
+      <div className="flex flex-col p-4 h-full rounded-lg border border-gray-200">
+        <div className="flex items-center mb-4">
+          <div
+            className={classNames(
+              `flex justify-center items-center w-12 h-12 rounded-full`,
+              isMe ? 'bg-indigo-300' : 'bg-indigo-100',
+            )}
+          >
+            <img
+              src={`https://avatars.dicebear.com/api/pixel-art/${message.name}.svg`}
+              alt={message.name}
+              className="overflow-hidden w-12 h-12 rounded-full"
+            />
+          </div>
+          <div className="flex flex-col grow pl-2">
+            <p className="text-lg font-medium">{message.name}</p>
+            <p className="text-xs text-gray-400">
+              {timestampToLocalString(message.timestamp)}
+            </p>
+          </div>
         </div>
-        <h2 className="mb-2 text-lg font-medium text-gray-900">
-          {message.name}
-        </h2>
-        <p className="text-base leading-relaxed">{message.message}</p>
+        <div>
+          <p>
+            {message.message}
+            とりあえずテキストを多めに流し込んで見るテストです。
+          </p>
+        </div>
+        <div className="flex justify-end items-center pt-4 mt-auto">
+          status toTask
+        </div>
       </div>
     </div>
   );
