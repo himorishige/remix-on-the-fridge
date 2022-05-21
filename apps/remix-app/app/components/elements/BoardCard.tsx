1d6c71a25f427b47cb983c9be153b8f0c770d7e5
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-21 19:09:38 +0900

    update: BoardCard

--- apps/remix-app/app/components/elements/BoardCard.tsx
+++ apps/remix-app/app/components/elements/BoardCard.tsx
@@ -1,18 +1,30 @@
 import type { Message } from 'board-do';
+import { useAtomValue } from 'jotai';
+import { usernameAtom } from '~/state/store';
 import { classNames } from '~/utils';
+import { Button } from '../ui';
 import { IntlDate } from './IntlDate';
+
+type Event = {
+  message: Message & { assignee: string };
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>;
+};
 
 type Props = {
   message: Message;
   isMe: boolean;
+  addTaskHandler: (params: Event) => void;
 };
 
-export const BoardCard: React.FC<Props> = ({ message, isMe }) => {
+export const BoardCard: React.FC<Props> = ({
+  message,
+  isMe,
+  addTaskHandler,
+}) => {
+  const username = useAtomValue(usernameAtom);
+
   return (
-    <div
-      className="p-2 w-full text-gray-700"
-      key={`${message.timestamp}${message.name}${message.message}`}
-    >
+    <div className="p-2 w-full text-gray-700">
       <div className="flex flex-col p-4 h-full bg-white rounded-lg border border-sky-400">
         <div className="flex items-center mb-4">
           <div
@@ -38,7 +50,52 @@ export const BoardCard: React.FC<Props> = ({ message, isMe }) => {
           <p>{message.message}</p>
         </div>
         <div className="flex justify-end items-center pt-4 mt-auto">
-          {message.status} toTask
+          <Button
+            onClick={(event) =>
+              addTaskHandler({
+                message: { ...message, assignee: username || 'anonymous' },
+                event,
+              })
+            }
+          >
+            <svg
+              width={18}
+              height={18}
+              fill="none"
+              viewBox="0 0 22 24"
+              className="mr-1"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M11.25 19.25H7.75C6.64543 19.25 5.75 18.3546 5.75 17.25V6.75C5.75 5.64543 6.64543 4.75 7.75 4.75H14L18.25 9V11.25"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M17 14.75V19.25"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M19.25 17L14.75 17"
+              />
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M18 9.25H13.75V5"
+              />
+            </svg>
+            <span className="text-sm">Convert to Task</span>
+          </Button>
         </div>
       </div>
     </div>
