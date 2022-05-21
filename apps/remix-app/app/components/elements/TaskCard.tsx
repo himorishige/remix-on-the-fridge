7c99deb1f17cfd9a2a9aa4c28b117345b0038d37
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-21 19:09:26 +0900

    update: TaskCard

--- apps/remix-app/app/components/elements/TaskCard.tsx
+++ apps/remix-app/app/components/elements/TaskCard.tsx
@@ -1,44 +1,56 @@
-import type { Message } from 'board-do';
+import type { Task } from 'board-do';
 import { classNames } from '~/utils';
 import { IntlDate } from './IntlDate';
 
 type Props = {
-  message: Message;
-  isMe: boolean;
+  task: Task;
 };
 
-export const TaskCard: React.FC<Props> = ({ message, isMe }) => {
+export const TaskCard: React.FC<Props> = ({ task }) => {
+  const isMe = false;
   return (
-    <div
-      className="p-2 w-full text-gray-700"
-      key={`${message.timestamp}${message.name}${message.message}`}
-    >
-      <div className="flex flex-col p-4 h-full bg-white rounded-lg border border-sky-400">
+    <div className="w-full text-gray-700">
+      <div className="flex flex-col p-4 h-full bg-white border-y border-y-sky-400">
         <div className="flex items-center mb-4">
-          <div
-            className={classNames(
-              `flex justify-center items-center w-12 h-12 rounded-full`,
-              isMe ? 'bg-indigo-300' : 'bg-indigo-100',
-            )}
-          >
-            <img
-              src={`https://avatars.dicebear.com/api/pixel-art/${message.name}.svg`}
-              alt={message.name}
-              className="overflow-hidden w-12 h-12 rounded-full"
-            />
+          <div className="flex items-center">
+            <div
+              className={classNames(
+                `flex justify-center items-center w-8 h-8 rounded-full`,
+                isMe ? 'bg-indigo-300' : 'bg-indigo-100',
+              )}
+            >
+              <img
+                src={`https://avatars.dicebear.com/api/pixel-art/${task.owner}.svg`}
+                alt={task.owner}
+                className="overflow-hidden w-8 h-8 rounded-full"
+              />
+            </div>
+            →
+            <div
+              className={classNames(
+                `flex justify-center items-center w-8 h-8 rounded-full`,
+                isMe ? 'bg-indigo-300' : 'bg-indigo-100',
+              )}
+            >
+              <img
+                src={`https://avatars.dicebear.com/api/pixel-art/${task.assignee}.svg`}
+                alt={task.assignee}
+                className="overflow-hidden w-8 h-8 rounded-full"
+              />
+            </div>
           </div>
           <div className="flex flex-col grow pl-2">
-            <p className="text-lg font-medium">{message.name}</p>
+            <p className="text-lg font-medium">{task.assignee}</p>
             <p className="text-xs text-gray-400">
-              <IntlDate date={new Date(message.timestamp)} />
+              <IntlDate date={new Date(task.timestamp)} />
             </p>
           </div>
         </div>
         <div>
-          <p>{message.message}</p>
+          <p>{task.title}</p>
         </div>
         <div className="flex justify-end items-center pt-4 mt-auto">
-          {message.status} toTask
+          {task.status} toTask
         </div>
       </div>
     </div>
