03c1efdfa98a2b985a5c44f32b1fb4ee0b8bdbf1
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-21 19:08:32 +0900

    add: utils

--- apps/remix-app/app/utils.ts
+++ apps/remix-app/app/utils.ts
@@ -1,3 +1,5 @@
+import type { Task } from 'board-do';
+
 export const normalizeBoardName = (board: string) => {
   return board
     .replace(/[^a-zA-Z0-9_-]/g, '')
@@ -11,4 +13,16 @@ export const timestampToLocalString = (timestamp: number) => {
 
 export const classNames = (...classes: unknown[]): string => {
   return classes.filter(Boolean).join(' ');
+};
+
+export const isPending = (status: Task['status']) => {
+  return status === 'pending';
+};
+
+export const isAssigned = (status: Task['status']) => {
+  return status === 'assigned';
+};
+
+export const isCompleted = (status: Task['status']) => {
+  return status === 'done';
 };
