f44cd6e30e544b1cab1ca2375b3420b6ad77e3cb
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-21 19:09:11 +0900

    update: Button component

--- apps/remix-app/app/components/ui/Button.tsx
+++ apps/remix-app/app/components/ui/Button.tsx
@@ -11,7 +11,7 @@ export const Button: React.FC<Props> = (props) => {
     <button
       {...props}
       className={classNames(
-        'py-2 px-4 text-white bg-blue-500 hover:bg-blue-600 disabled:bg-slate-400 rounded border-0 focus:outline-none',
+        'py-2 px-4 text-white bg-blue-500 hover:bg-blue-600 disabled:bg-slate-400 rounded border-0 focus:outline-none flex items-center justify-center',
         props.full ? 'w-full' : '',
         props.secondary
           ? 'bg-cyan-500 hover:bg-cyan-600'
