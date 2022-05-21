611f30256164b4514606ea51ca907c266ee64fa4
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-21 12:31:24 +0900

    remove: unused atom

--- apps/remix-app/app/state/store.ts
+++ apps/remix-app/app/state/store.ts
@@ -5,8 +5,4 @@ export const boardIdAtom = atom<string | null>(null);
 export const boardLoaderCallsAtom = atom<number>(0);
 export const usernameAtom = atom<string | null>(null);
 export const newMessageAtom = atom<Message[]>([]);
-export const socketAtom = atom<WebSocket | null>(null);
 export const userListAtom = atom<string[]>([]);
-
-// input form
-export const inputValueAtom = atom<string>('');
