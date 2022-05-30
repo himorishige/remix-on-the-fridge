bc533b25837f0f6deaa48cce0719b5c820773aa0
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-30 22:47:26 +0900

    fix: layout

--- apps/remix-app/app/components/layout/Footer.tsx
+++ apps/remix-app/app/components/layout/Footer.tsx
@@ -11,22 +11,22 @@ export const Footer: React.FC<Props> = (props) => {
   return (
     <footer>
       <div className="bg-sky-700">
-        <div className="container flex flex-wrap py-4 mx-auto">
-          <p className="text-sm text-center text-gray-200 sm:text-left">
+        <div className="flex flex-wrap justify-between items-center p-4 mx-auto">
+          <p className="text-sm text-gray-200">
             &copy; 2022 _himorishige —
             <a
               href="https://twitter.com/_himorishige"
-              className="ml-1 text-gray-100"
+              className="text-gray-100"
               target="_blank"
               rel="noopener noreferrer"
             >
               @_himorishige
             </a>
           </p>
-          <span className="mt-2 w-full text-sm text-center text-gray-300 sm:mt-0 sm:ml-auto sm:w-auto sm:text-left">
+          <div className="text-sm text-gray-300">
             Root loader invocations: {props.loaderCalls}. Local loader
             invocations: {localLoaderCalls}.
-          </span>
+          </div>
         </div>
       </div>
     </footer>
