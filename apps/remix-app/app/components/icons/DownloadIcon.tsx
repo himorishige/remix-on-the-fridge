63f317c3c409467113ca668535b1c45dee70add2
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-06-01 16:36:45 +0900

    add: sticky export function

---
+++ apps/remix-app/app/components/icons/DownloadIcon.tsx
@@ -0,0 +1,21 @@
+import type { ComponentProps } from 'react';
+
+type Props = ComponentProps<'svg'>;
+
+export const DownloadIcon: React.FC<Props> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-5 h-5"
+    fill="none"
+    viewBox="0 0 22 22"
+    stroke="currentColor"
+    strokeWidth={2}
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+    />
+  </svg>
+);
--- apps/remix-app/app/components/layout/StickyArea.tsx
+++ apps/remix-app/app/components/layout/StickyArea.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { SelectorIcon, FilterIcon, CheckIcon } from '~/components/icons';
 import { useAtomValue } from 'jotai';
@@ -6,6 +6,9 @@ import { newTaskAtom } from '~/state/store';
 import type { Task } from 'board-do';
 import { TaskCard } from '~/components/elements';
 import type ReconnectingWebSocket from 'reconnecting-websocket';
+import html2canvas from 'html2canvas';
+import { Button } from '../ui';
+import { DownloadIcon } from '../icons/DownloadIcon';
 
 type Props = {
   socket: ReconnectingWebSocket | null;
@@ -20,6 +23,34 @@ export type CompleteTaskEvent = {
 export const StickyArea: React.FC<Props> = ({ socket, latestTasks }) => {
   const newTasks = useAtomValue(newTaskAtom);
   const [latestTasksStore, setLatestTasksStore] = useState(latestTasks);
+
+  const target = useRef<HTMLDivElement>(null);
+
+  const onClickExport = () => {
+    if (target.current) {
+      html2canvas(target.current, {
+        backgroundColor: 'rgba(186, 230, 253, 1)',
+        useCORS: true,
+      }).then((canvas) => {
+        const targetImgUri = canvas.toDataURL('img/png');
+        saveAsImage(targetImgUri);
+      });
+    }
+  };
+
+  const saveAsImage = (uri: string) => {
+    const downloadLink = document.createElement('a');
+
+    if (typeof downloadLink.download === 'string') {
+      downloadLink.href = uri;
+      downloadLink.download = 'sticky-notes.png';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      window.open(uri);
+    }
+  };
 
   const [people, setPeople] = useState([
     'all',
@@ -55,9 +86,9 @@ export const StickyArea: React.FC<Props> = ({ socket, latestTasks }) => {
 
   return (
     <>
-      <div className="p-2 pt-5">
+      <div className="flex justify-between items-center p-2 pt-5 pr-4 w-full">
         <Listbox value={selected} onChange={setSelected}>
-          <div className="relative">
+          <div className="relative pr-4 w-full">
             <Listbox.Button className="relative py-2 pr-10 pl-3 w-full text-left bg-white rounded-lg focus-visible:border-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 shadow-md cursor-default sm:text-sm">
               <span className="block truncate">
                 <span className="flex items-center text-gray-700">
@@ -113,31 +144,37 @@ export const StickyArea: React.FC<Props> = ({ socket, latestTasks }) => {
             </Transition>
           </div>
         </Listbox>
+        <Button type="button" onClick={onClickExport}>
+          <DownloadIcon />
+          <span className="ml-1">export</span>
+        </Button>
       </div>
-      {latestTasks.length === 0 && newTasks.length === 0 ? (
-        <div className="p-3 text-center">
-          <p className="text-gray-500">No stickies</p>
+      <div ref={target}>
+        {latestTasks.length === 0 && newTasks.length === 0 ? (
+          <div className="p-3 text-center">
+            <p className="text-gray-500">No stickies</p>
+          </div>
+        ) : null}
+        <div className="grid gap-2 p-2 sm:grid-cols-2 xl:grid-cols-3">
+          {newTasks
+            .filter((task) => task.assignee === selected || selected === 'all')
+            .map((task) => (
+              <TaskCard
+                key={task.id}
+                task={task}
+                completeTaskHandler={completeTaskHandler}
+              />
+            ))}
+          {latestTasksStore
+            .filter((task) => task.assignee === selected || selected === 'all')
+            .map((task) => (
+              <TaskCard
+                key={task.id}
+                task={task}
+                completeTaskHandler={completeTaskHandler}
+              />
+            ))}
         </div>
-      ) : null}
-      <div className="grid gap-2 p-2 sm:grid-cols-2 xl:grid-cols-3">
-        {newTasks
-          .filter((task) => task.assignee === selected || selected === 'all')
-          .map((task) => (
-            <TaskCard
-              key={task.id}
-              task={task}
-              completeTaskHandler={completeTaskHandler}
-            />
-          ))}
-        {latestTasksStore
-          .filter((task) => task.assignee === selected || selected === 'all')
-          .map((task) => (
-            <TaskCard
-              key={task.id}
-              task={task}
-              completeTaskHandler={completeTaskHandler}
-            />
-          ))}
       </div>
     </>
   );
